1b52bfdc53a07267d2e3f14132bbee8a953920ad
CarsonBain <carsonbain@gmail.com>
2018-11-22 22:29:51 -0800

    Move JS to external file and reconfigure

---
+++ js/main.js
@@ -0,0 +1,67 @@
+var soundProps = [
+  {
+    soundID : "bitchdab",
+    soundName : "Bitch, dab"
+  },
+  {
+    soundID : "versace",
+    soundName : "Versace"
+  },
+  {
+    soundID: "pipeitup",
+    soundName: "Pipe it up"
+  },
+  {
+    soundID: "chicken",
+    soundName: "Chicken"
+  },
+  {
+    soundID: "hannah",
+    soundName: "Hannah Montana"
+  },
+  {
+    soundID: "gah",
+    soundName: "Gah, Gah, Gah"
+  },
+  {
+    soundID: "realdeal",
+    soundName: "Real Deal"
+  },
+  {
+    soundID: "migos",
+    soundName: "Migos"
+  },
+  {
+    soundID: "leftoff",
+    soundName: "Left Off"
+  },
+  {
+    soundID: "splash",
+    soundName: "Splash"
+  },
+  {
+    soundID: "skrskr",
+    soundName: "Skrr, skrr"
+  },
+  { 
+    soundID: "slippery",
+    soundName: "Slippery"
+  },
+  {
+    soundID: "bitch",
+    soundName: "Bitch!"
+  }
+];
+soundProps.forEach(function(element) {
+  var audio = document.createElement('audio');
+  audio.src = 'audio/' + element.soundID +'.m4a';
+  audio.id = element.soundID;
+  var button = document.createElement('button');
+  button.classList = element.soundID;
+  button.textContent = element.soundName;
+  button.onclick = function(){
+    document.getElementById(element.soundID).play();
+  };
+  document.body.appendChild(audio);
+  document.getElementById('sound-wrap').appendChild(button);
+});
